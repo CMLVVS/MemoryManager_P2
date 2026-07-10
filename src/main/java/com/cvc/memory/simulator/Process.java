172f56f102cc39059fe0e2545631c3280d58c0ce
CMLVVS <camilo.vivas.correa@correounivalle.edu.co>
2026-07-10 14:23:55 -0500

    Clase Process

---
+++ src/main/java/com/cvc/memory/simulator/Process.java
@@ -0,0 +1,15 @@
+package com.cvc.memory.simulator;
+
+public class Process {
+    private String id;
+    private int size;
+
+    public Process(String id, int size) {
+        this.id = id;
+        this.size = size;
+    }
+
+    public String getId() { return id; }
+    public int getSize() { return size; }
+}
+
